d486b6bd1ec475a7a2caccbdb32bdd15ec8d131d
eminamumic <emimumic1@gmail.com>
2025-08-20 20:32:45 +0200

    Add category array and difficulty array in data.ts

--- src/data.ts
+++ src/data.ts
@@ -21,3 +21,58 @@ export async function getData(
     console.error('Error fetching data:', error)
   }
 }
+
+export const categories = [
+  {
+    name: 'Science',
+    imageSrc: '/materijal/category/sci.png',
+    value: 'science',
+  },
+  {
+    name: 'Film',
+    imageSrc: '/materijal/category/film.png',
+    value: 'film_and_tv',
+  },
+  {
+    name: 'Art & Literature',
+    imageSrc: '/materijal/category/art.png',
+    value: 'arts_and_literature',
+  },
+  {
+    name: 'History',
+    imageSrc: '/materijal/category/history.png',
+    value: 'history',
+  },
+  {
+    name: 'Society & Culture',
+    imageSrc: '/materijal/category/soc.png',
+    value: 'society_and_culture',
+  },
+  {
+    name: 'Geography',
+    imageSrc: '/materijal/category/geo.png',
+    value: 'geography',
+  },
+  {
+    name: 'Food & Drink',
+    imageSrc: '/materijal/category/food.png',
+    value: 'food_and_drink',
+  },
+  {
+    name: 'General Knowledge',
+    imageSrc: '/materijal/category/knowledge.png',
+    value: 'general_knowledge',
+  },
+  { name: 'Music', imageSrc: '/materijal/category/music.png', value: 'music' },
+  {
+    name: 'Sport',
+    imageSrc: '/materijal/category/sport.png',
+    value: 'sport_and_leisure',
+  },
+]
+
+export const difficulties = [
+  { name: 'Easy', value: 'easy' },
+  { name: 'Medium', value: 'medium' },
+  { name: 'Hard', value: 'hard' },
+]
